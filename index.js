7e75f1a47ce66dc10cd90c8eca088c11b615b70d
throdi <crysaorkappa@gmail.com>
2018-11-15 20:05:51 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'hello') {
+    msg.reply('same to you sir');
+  }
+});
+
+client.login(config.token);
