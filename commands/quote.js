f1740bb4e91809297180c4d6a92427b98ac9838f
throdi <crysaorkappa@gmail.com>
2018-11-15 21:59:07 +0100

    basic config and shit

---
+++ commands/quote.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'quote',
+    description: 'Add your own quote or hear random stuff',
+    execute(message, args) {
+        message.channel.send('Working on it chief!');
+    },
+};
--- index.js
+++ index.js
@@ -1,17 +1,37 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-const config = require('./config.json');
-
+const { prefix, nicknameprefix, token} = require('./config.json');
 
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'hello') {
-    msg.reply('same to you sir');
+client.on('message', message => {
+
+  if ((!message.content.startsWith(prefix) && !message.content.startsWith(nicknameprefix))  || message.author.bot) return;
+
+  const args = message.content.slice(message.content.startsWith(prefix) ? prefix.length+1 : nicknameprefix.length+1).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (!client.commands.has(command)) return;
+
+  try {
+      client.commands.get(command).execute(message, args);
   }
+  catch (error) {
+      console.error(error);
+      message.reply('shut up');
+  }
+
 });
 
-client.login(config.token);
+client.login(token);
