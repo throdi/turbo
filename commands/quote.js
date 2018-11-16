24ee721e30fca132872097ed48cb7add12190235
throdi <crysaorkappa@gmail.com>
2018-11-16 22:46:27 +0100

    Storing  user quotes

--- commands/quote.js
+++ commands/quote.js
@@ -1,7 +1,93 @@
+const fs = require("fs");
+existingIdentifiers = [];
+
 module.exports = {
-    name: 'quote',
-    description: 'Add your own quote or hear random stuff',
-    execute(message, args) {
-        message.channel.send('Working on it chief!');
-    },
+  name: "quote",
+  usage: "@<mention> quote <quote_id | add | remove> (quote that follows add or id to remove)",
+  description: "Add your own quote or hear random stuff",
+  execute(message, args) {
+    reloadData();
+
+    // ex: @Turbo quote
+    if (args.length == 0) return message.channel.send(randomQuote());
+
+    const mainArg = args.shift().toLowerCase();
+
+    if (mainArg === "add") {
+      // @Turbo quote add <args>
+      if (args.length == 0) {
+        //No args, nothing to add, scold the user
+        return message.channel.send("WHAT DO YOU WANT ME TO ADD?!?!?");
+      } else {
+        const identifier = args.shift().toLowerCase();
+
+        if (!identifierExists(identifier)) {
+          return message.channel.send("Sorry, that already exists!");
+        }
+
+        if (args.length == 0) {
+          //The user clearly doesn't know how to use this command, explain the usage
+          return message.channel.send("add WHAT?!?!?"); // I'm too lazy.
+        } else {
+          const quote = args.join(" ");
+          message.channel.send("Gotcha fam, I grow smarter.");
+          saveFile(message, identifier, quote);
+        }
+      }
+    } else if (mainArg == "remove") {
+      // todo
+    } else {
+      if (!identifierExists(mainArg)) {
+        let content = fs.readFileSync("./quotes/" + mainArg + ".json");
+        const quoteData = JSON.parse(content);
+        return message.channel.send(quoteData.quote);
+      } else {
+        return message.channel.send("Such quote doesn't exist you dingus");
+      }
+    }
+  }
+};
+
+const identifierExists = identifier => {
+  if (existingIdentifiers.indexOf(identifier) > -1) return false;
+  return true;
+};
+
+const saveFile = (message, identifier, quote) => {
+  const jsonObj = {
+    owner: message.author.id,
+    date: new Date(),
+    name: identifier,
+    quote: quote
+  };
+
+  fs.writeFile(
+    "./quotes/" + identifier + ".json",
+    JSON.stringify(jsonObj),
+    function(err) {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log(`The quote ${quote} was saved!`);
+    }
+  );
+};
+
+const reloadData = () => {
+  const identifiers = fs
+    .readdirSync("./quotes")
+    .filter(file => file.endsWith(".json"));
+  existingIdentifiers = [];
+  for (const file of identifiers) {
+    existingIdentifiers.push(file.substring(0, file.length - 5));
+  }
+};
+
+const randomQuote = () => {
+  let randomId =
+    existingIdentifiers[Math.floor(Math.random() * existingIdentifiers.length)];
+  let content = fs.readFileSync("./quotes/" + randomId + ".json");
+  let quoteData = JSON.parse(content);
+  return quoteData.quote;
 };
--- index.js
+++ index.js
@@ -1,37 +1,55 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const { prefix, nicknameprefix, token} = require('./config.json');
+const fs = require("fs");
+const Discord = require("discord.js");
+const { prefix, nicknameprefix, token } = require("./config.json");
+//const sniffFunnyWords = require("./sniff-funny-words.js");
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter(file => file.endsWith(".js"));
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
 }
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
+client.on("message", message => {
+  //if (!message.author.bot) sniffFunnyWords.execute(message);
 
-  if ((!message.content.startsWith(prefix) && !message.content.startsWith(nicknameprefix))  || message.author.bot) return;
+  if (
+    (!message.content.startsWith(prefix) &&
+      !message.content.startsWith(nicknameprefix)) ||
+    message.author.bot
+  )
+    return;
 
-  const args = message.content.slice(message.content.startsWith(prefix) ? prefix.length+1 : nicknameprefix.length+1).split(/ +/);
-  const command = args.shift().toLowerCase();
+  const args = message.content
+    .slice(
+      message.content.startsWith(prefix)
+        ? prefix.length + 1
+        : nicknameprefix.length + 1
+    )
+    .split(/ +/);
+  //if added more than one space after
+  if (args[0] == "") args.shift();
 
-  if (!client.commands.has(command)) return;
+  const commandName = args.shift().toLowerCase();
+
+  if (!client.commands.has(commandName)) return;
+
+  const command = client.commands.get(commandName);
 
   try {
-      client.commands.get(command).execute(message, args);
+    command.execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("shut up");
   }
-  catch (error) {
-      console.error(error);
-      message.reply('shut up');
-  }
-
 });
 
 client.login(token);
